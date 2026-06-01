ec3ddcc49d98c095887e8406e6c04faf933a78e7
Azamat1303 <abduazamat448@gmail.com>
2026-06-01 17:29:52 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os
- 
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
- 
+
 ADMIN_IDS = [6060306988]
- 
+
 QUESTION_TIME = 30
- 
+
 PENALTY_PERCENT = 0.3
- 
+
 TIMEOUT_PENALTY = 0.45
- 
+
 STREAK_BONUSES = {
     3:  1.5,
     5:  2.0,
     10: 3.0,
 }
- 
+
 GROQ_API_KEY = "AQ.Ab8RN6KZCBn2FW9HDanaLU8wBhbjJZJjSoguSc6csDL_8ndPBw"
 GROQ_MODEL = "llama-3.3-70b-versatile"
