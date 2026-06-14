8e1ce9b0d25c827b4bfb88f73d9a4ac281ce6c97
Azamat1303 <abduazamat448@gmail.com>
2026-06-14 19:46:23 +0500

    Update config.py

--- config.py
+++ config.py
@@ -17,3 +17,4 @@ STREAK_BONUSES = {
 GROQ_API_KEY = "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"
 GROQ_MODEL = "llama3-70b-8192"
 GEMINI_API_KEY = "AQ.Ab8RN6LZScKDk8xS6t1LUkPA_IPRN9Cegt_CJBC5MrvwLTdUug"
+DATABASE_URL ="postgresql://neondb_owner:npg_ISTdnGO5YpE3@ep-crimson-scene-aqmmilfp-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
