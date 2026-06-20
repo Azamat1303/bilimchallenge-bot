f18098aff8271a871729f9e2f1061f37d9439a11
Azamat1303 <abduazamat448@gmail.com>
2026-06-20 16:24:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
- 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-BOT_TOKEN = "8930806821:AAGQTTeZow7y1bX_FfLhrRPj89XwcT8gDFM"
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8930806821:AAGQTTeZow7y1bX_FfLhrRPj89XwcT8gDFM")
 ADMIN_IDS = [6060306988]
 
 QUESTION_TIME = 30
@@ -14,7 +13,6 @@ STREAK_BONUSES = {
     10: 3.0,
 }
 
-GROQ_API_KEY = "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"
-GROQ_MODEL = "llama3-70b-8192"
-GEMINI_API_KEY = "AQ.Ab8RN6LZScKDk8xS6t1LUkPA_IPRN9Cegt_CJBC5MrvwLTdUug"
-DATABASE_URL = "postgresql://neondb_owner:npg_ISTdnGO5YpE3@ep-crimson-scene-aqmmilfp-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+# Groq API (Gemini olib tashlandi)
+GROQ_API_KEY = "xai-KQjuCy3m4qIUcWBPImP6wjwyuS0JBWwvW6oRje9mEH0JCE5q7475dDXKH7E4eIURytYYshQAHKctVVIb"
+GROQ_MODEL   = "llama-3.3-70b-versatile"
